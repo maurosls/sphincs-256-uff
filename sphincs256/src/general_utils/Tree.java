1f94ca35d5d29cb6ec27aee16d2b649a34a9a456
omarMR <34353194+omarMR@users.noreply.github.com>
2017-12-11 21:19:17 -0200

    Initial code of Tree

--- sphincs256/src/general_utils/Tree.java
+++ sphincs256/src/general_utils/Tree.java
@@ -5,7 +5,9 @@
  */
 package general_utils;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.math.*;
 
 /**
  *
@@ -13,4 +15,17 @@ import java.util.List;
  */
 public class Tree {
     
+    public /*verificar tipo de retorno*/void hashTree(List<byte[]> leafs){
+        assert (leafs.size() & leafs.size()-1)==0;
+        return lTree(leafs);
+    }
+    
+    public /*verificar tipo de retorno*/void lTree(List<byte[]> leafs){
+        List<byte[]> layer = new ArrayList<>(leafs);
+        //yield layer
+        for (int i = 0; i < (int)(Math.ceil(Math.log(leafs.size())/Math.log(2))); i++) {
+            List<byte[]> nextLayer = new ArrayList<>();
+            
+        }
+    }
 }
