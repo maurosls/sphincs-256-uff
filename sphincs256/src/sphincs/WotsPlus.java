9dae86225f3b9402617df519da821cd69394c9b8
R. Bern <raphaelbernardino@users.noreply.github.com>
2017-12-12 23:43:35 -0200

    Sugestão de modificação no WOTS+
    Talvez seja necessário usar essas duas funções comentadas (int_to_basew, chainSizes), mas não vejo para que usar. Elas são feitas para calcular a representação nos passos de assinar e verificar.

--- sphincs256/src/sphincs/WotsPlus.java
+++ sphincs256/src/sphincs/WotsPlus.java
@@ -1,54 +1,85 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sphincs;
 
 import java.math.*;
 import java.util.List;
+
 /**
- *
- * @author Eu
+ * @author Bernardino
  */
-public class WotsPlus {
-    
+
+public class WotsPlus
+{
+    // tamanho dos hash
     private int bitsHashes;
+    
+    // parametro de winternitz para criação da estrutrua
     private int paramWinternitz;
-    private int l1;//não sei o que é
-    private int l2;//não sei o que é
-    private int l;//não sei o que é
     
-    public WotsPlus (int bitsHashes, int paramWinternitz){
+    // wots+ proposto por bernstein (pagina 7 do artigo WOTS+)
+    private int l1;
+    private int l2;
+    
+    // quantidade de chaves pseudo-aleatorias que serão geradas; sphincs-256 ==> L = 67 = L1 + L2 = 64 + 3
+    private int l;
+    
+    public WotsPlus (int bitsHashes, int paramWinternitz)
+    {
         this.bitsHashes = bitsHashes;
+        
         this.paramWinternitz = paramWinternitz;
-        this.l1 = (int)(Math.ceil(bitsHashes/
-                                  (int)(Math.log(paramWinternitz)/Math.log(2))));
-        this.l2 = (int)(Math.floor((int)(Math.log(this.l1*(paramWinternitz-1))/Math.log(2))/
-                                   (int)(Math.log(paramWinternitz)/Math.log(2))))+1;
-        this.l = this.l1+this.l2;
+        
+        this.l1 = (int)(Math.ceil(bitsHashes/(int)(Math.log(paramWinternitz)/Math.log(2))));
+        this.l2 = (int)(Math.floor((int)(Math.log(this.l1*(paramWinternitz-1))/Math.log(2))/(int)(Math.log(paramWinternitz)/Math.log(2))))+1;
+        
+        this.l = this.l1 + this.l2;
     }
     
-    public /*verificar tipo de retorno*/ void chains(/*x, masks, chainRange*/){
+    // Chaining function descrita na página 7 do artigo
+    private byte[] chains(byte[] x, byte[] masks)
+    {
+        // 1. Para cada round Li ate L, faça
+        // 1.1. Calcule recursivamente, c[i-1] XOR r[i]
+        // 1.2. Aplique a função de hash F e atribua a c[i]
+        // 2. Retorne c
     }
 
-    public /*verificar tipo de retorno*/ void int_to_basew(/*x, base*/){
-    }
+    // Não sei se precisa disso
+    public /*verificar tipo de retorno*/ void int_to_basew(/*x, base*/){}
 
-    public /*verificar tipo de retorno*/ void chainSizes(/*message*/){
-    }
-
-    public /*verificar tipo de retorno*/ void keygen(byte[] seed, List<byte[]> masks){
-    }
-
-    public /*verificar tipo de retorno*/ void sign(/*message,*/byte[] seed, List<byte[]> masks){
-    }
-
-    public /*verificar tipo de retorno*/ void verify(/*message, signature, */List<byte[]> masks){
-    }
-
-    public int getL() {
-        return l;
-    }
+    // Nao sei se precisa disso
+    public /*verificar tipo de retorno*/ void chainSizes(/*message*/){}
     
+    // Gera a chave publica referente a arvore. (talvez possa retornar uma String Hexadecimal, caso seja mais facil)
+    public byte[][] keygen(byte[] seed, List<byte[]> masks)
+    {
+        // 1. Gera a chave secreta {SK} utilizando a seed. (em bytes)
+        // 2. Divide a chave secreta {SK} em pedaços de 8 bytes. (em bytes)
+        // 3. Para cada chave publica PKi (de 1 ate L) sera gerada uma chave correspondente a aplicacao da funcao {chains} com os parametros {SKi} e a mascara de bits {masks}.
+        // 3.1. observacao: pk = lista de PKi's, onde cada PKi = chains(SKi, r)
+        // 3.2. por exemplo: PK1 = chains(SK1, r)
+    }
+
+    // Assina as mensagens usando a arvore wots+
+    // (*) a mensagem pode ser interpretada como bytes
+    public byte[] void sign(String message, byte[] seed, List<byte[]> masks)
+    {
+        // 1. Gera a chave secreta {SK} utilizando a seed. (em bytes)
+        // 2. Divide a chave secreta {SK} em pedaços de 8 bytes. (em bytes)
+        // 3. Calcula-se a representação w-ária C (no máximo L2), fazendo, C = somatorio(w - 1 - Mi), onde Mi é um pedaço da mensagem.
+        // 4. Calcula-se B sendo a concatenação da representação de M e C. ( B = M concatenado com C)
+        // 5. Para cada Bi em B, aplique a função {chain} com os parametros {SKi} e {masks}
+    }
+
+    // Verifica se a assinatura eh valida. Pagina 8 do artigo (Verification algorithm).
+    public boolean void verify(/*message, signature, */List<byte[]> masks)
+    {
+        // 1. Calcule B seguindo os passos de {sign} e faça
+        // 2. De Bi até B_w-1 faça, {chain} com os parametros {signature} e {masks} (e guarde essa informação em PK_)
+        // 3. Retorne verdadeiro caso a assinatura seja valida, e falso caso contrário. (comparando a chave publica PK_ com PK, se necessario)
+    }
+
+    public int getL()
+    {
+        return this.l;
+    }
 }
